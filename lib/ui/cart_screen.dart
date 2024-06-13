64db9893c771ce201ec8e84a565c6d1cf0818bbb
inirafli <raplirayhann@gmail.com>
2024-06-13 22:09:25 +0700

    modified styling of the widgets inside cart screen

--- lib/ui/cart_screen.dart
+++ lib/ui/cart_screen.dart
@@ -1,7 +1,11 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:provider/provider.dart';
 
 import '../provider/cart_provider.dart';
+import '../utils/data_conversion.dart';
 
 class CartScreen extends StatefulWidget {
   const CartScreen({super.key});
@@ -21,81 +25,189 @@ class _CartScreenState extends State<CartScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Keranjang'),
-      ),
-      body: Consumer<CartProvider>(
-        builder: (context, cartProvider, child) {
-          if (cartProvider.cartItems.isEmpty) {
-            return Center(
-              child: Text(
-                'Keranjang Kosong',
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
-            );
-          }
+    Color primary = Theme.of(context).colorScheme.primary;
+    Color secondary = Theme.of(context).colorScheme.secondary;
+    Color onPrimary = Theme.of(context).colorScheme.onPrimary;
+    Color onBackground = Theme.of(context).colorScheme.onBackground;
 
-          return ListView.builder(
-            itemCount: cartProvider.cartItems.length,
-            itemBuilder: (context, index) {
-              final item = cartProvider.cartItems[index];
-              return ListTile(
-                leading: Image.network(item.image, width: 50, height: 50, fit: BoxFit.cover),
-                title: Text(item.name),
-                subtitle: Text('Rp${item.price} x ${item.quantity}'),
-                trailing: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    IconButton(
-                      icon: const Icon(Icons.remove),
-                      onPressed: () {
-                        if (item.quantity > 1) {
-                          cartProvider.updateCartItemQuantity(item.id, item.quantity - 1);
-                        }
-                      },
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text(
+          'Keranjang',
+          style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                fontWeight: FontWeight.bold,
+                color: onPrimary,
+              ),
+        ),
+      ),
+      body: Stack(
+        children: [
+          Consumer<CartProvider>(
+            builder: (context, cartProvider, child) {
+              if (cartProvider.cartItems.isEmpty) {
+                return Center(
+                  child: Text(
+                    'Keranjang Kosong',
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                );
+              }
+
+              return ListView.builder(
+                padding: const EdgeInsets.only(bottom: 120.0, top: 16.0),
+                itemCount: cartProvider.cartItems.length,
+                itemBuilder: (context, index) {
+                  final item = cartProvider.cartItems[index];
+                  return Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                    decoration: BoxDecoration(
+                      color: onPrimary,
+                      borderRadius: BorderRadius.circular(10.0),
                     ),
-                    IconButton(
-                      icon: const Icon(Icons.add),
-                      onPressed: () {
-                        cartProvider.updateCartItemQuantity(item.id, item.quantity + 1);
-                      },
+                    child: Row(
+                      children: [
+                        ClipRRect(
+                          borderRadius: const BorderRadius.only(topLeft: Radius.circular(10), bottomLeft: Radius.circular(10)),
+                          child: Image.network(
+                            item.image,
+                            width: 100,
+                            height: 120,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        const SizedBox(width: 8.0),
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  item.name,
+                                  style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                                    fontWeight: FontWeight.bold,
+                                    color: primary,
+                                  ),
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                                const SizedBox(height: 0.5),
+                                Text(
+                                  formatPrice(item.price),
+                                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                                    color: onBackground,
+                                  ),
+                                ),
+                                const SizedBox(height: 2.0),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Container(),
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      children: [
+                                        IconButton(
+                                          icon: Icon(Icons.delete_outline, size: 23.5, color: primary),
+                                          onPressed: () {
+                                            cartProvider.removeFromCart(item.id);
+                                          },
+                                        ),
+                                        IconButton(
+                                          icon: Icon(MdiIcons.minusCircleOutline, size: 23.0, color: primary),
+                                          onPressed: () {
+                                            if (item.quantity > 1) {
+                                              cartProvider.updateCartItemQuantity(
+                                                  item.id, item.quantity - 1);
+                                            }
+                                          },
+                                        ),
+                                        const SizedBox(width: 4.0),
+                                        Text(
+                                          '${item.quantity}',
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .bodyLarge
+                                              ?.copyWith(
+                                            fontWeight: FontWeight.bold,
+                                            color: primary,
+                                          ),
+                                        ),
+                                        const SizedBox(width: 4.0),
+                                        IconButton(
+                                          icon: Icon(MdiIcons.plusCircleOutline, size: 23.0, color: primary),
+                                          onPressed: () {
+                                            cartProvider.updateCartItemQuantity(
+                                                item.id, item.quantity + 1);
+                                          },
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                    IconButton(
-                      icon: const Icon(Icons.delete),
-                      onPressed: () {
-                        cartProvider.removeFromCart(item.id);
-                      },
-                    ),
-                  ],
-                ),
+                  );
+                },
               );
             },
-          );
-        },
-      ),
-      bottomNavigationBar: Consumer<CartProvider>(
-        builder: (context, cartProvider, child) {
-          return Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  'Total: Rp${cartProvider.totalPrice}',
-                  style: Theme.of(context).textTheme.titleLarge,
-                ),
-                const SizedBox(height: 16.0),
-                ElevatedButton(
-                  onPressed: () {
-                    // Implement the request submission functionality
-                  },
-                  child: const Text('Submit Request'),
-                ),
-              ],
+          ),
+          Positioned(
+            bottom: 16,
+            left: 16,
+            right: 16,
+            child: Container(
+              color: onPrimary,
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Total Harga',
+                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                              color: onBackground,
+                            ),
+                      ),
+                      Text(
+                        formatPrice(Provider.of<CartProvider>(context)
+                            .totalPrice
+                            .toInt()),
+                        style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                              color: primary,
+                              fontWeight: FontWeight.bold,
+                            ),
+                      ),
+                    ],
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: const Size(132.0, 44.0),
+                      padding: EdgeInsets.zero,
+                    ),
+                    onPressed: () {
+                      // Implement the request submission functionality
+                    },
+                    child: Text(
+                      'Buat Pesanan',
+                      style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                            fontWeight: FontWeight.bold,
+                            color: onPrimary,
+                          ),
+                    ),
+                  )
+                ],
+              ),
             ),
-          );
-        },
+          ),
+        ],
       ),
     );
   }
